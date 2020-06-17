bfc410cc2908b36be607d8b2c846480432b70d99
thomas.bouchardon <thomas.bouchardon@developpement-durable.gouv.fr>
2020-06-17 15:14:24 +0200

    Ajout de Junit 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ repositories {
 
 dependencies {
 //    providedCompile("javax.servlet:javax.servlet-api:3.1.0")
-//    testImplementation("junit:junit:4.12")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.6.2")
     implementation("org.postgresql:postgresql:42.1.4")
 }
 
